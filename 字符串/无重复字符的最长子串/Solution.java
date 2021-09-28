dfadd0d537ae25c851e58c94a61fea22b4d824a8
chenxuyang02 <chenxuyang02@58.com>
2021-09-28 17:39:03 +0800

    无重复字符的最长子串暴力解法

--- 字符串/无重复字符的最长子串/Solution.java
+++ 字符串/无重复字符的最长子串/Solution.java
@@ -12,35 +12,37 @@ import java.util.Set;
 public class Solution {
 
     /**
-     * 思路：
-     * 使用两个指针表示字符串中的某个子串（或窗口）的左右边界，其中左指针代表着上文中「枚举子串的起始位置」，而右指针即为不包含重复字符的最长子串的结束位置;
-     *
-     * 在每一步的操作中，会将左指针向右移动一格，表示我们开始枚举下一个字符作为起始位置，然后我们可以不断地向右移动右指针，但需要保证这两个指针对应的子串中没有重复的字符。在移动结束后，这个子串就对应着 以左指针开始的，不包含重复字符的最长子串。我们记录下这个子串的长度；
-     *
-     * 在枚举结束后，我们找到的最长的子串的长度即为答案。
-     *
+     * 暴力解法
      * @param s
      * @return
      */
     public int lengthOfLongestSubstring(String s) {
-        // 记录每个字符是否出现过
-        Set<Character> occ = new HashSet<Character>();
-        int n = s.length();
-        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-        int rk = -1, ans = 0;
-        for (int i = 0; i < n; ++i) {
-            if (i != 0) {
-                // 左指针向右移动一格，移除一个字符
-                occ.remove(s.charAt(i - 1));
-            }
-            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
-                // 不断地移动右指针
-                occ.add(s.charAt(rk + 1));
-                ++rk;
-            }
-            // 第 i 到 rk 个字符是一个极长的无重复字符子串
-            ans = Math.max(ans, rk - i + 1);
+        HashSet set = new HashSet();
+        int len = s.length();
+        if (len == 0) {
+            return 0;
         }
-        return ans;
+        int temp = 1;
+        for (int i = 0; i < len; i++) {
+            set.clear();
+            char a = s.charAt(i);
+            set.add(a);
+            for (int j = i + 1; j < len; j++) {
+                char b = s.charAt(j);
+                boolean add = set.add(b);
+                if (!add) {
+                    if (j - i >= temp) {
+                        temp = j - i;
+                    }
+                    break;
+                }
+                if (j == len - 1) {
+                    if (j - i >= temp) {
+                        temp = j - i + 1;
+                    }
+                }
+            }
+        }
+        return temp;
     }
 }
