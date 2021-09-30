f86c12d12a0f4c459487ea7105b8c676e4214ee7
chenxuyang02 <chenxuyang02@58.com>
2021-09-30 10:29:00 +0800

    两数相加

--- 链表/两数相加/Solution.java
+++ 链表/两数相加/Solution.java
@@ -9,7 +9,7 @@ package 两数相加;
 public class Solution {
 
     /**
-     * 思路：暴力解法，转成数组
+     * 思路：同时遍历两个链表，逐位计算它们的和，并与当前位置的进位值相加。数据存入数组，最后转成链表
      *
      * @param nums
      * @param target
@@ -81,6 +81,93 @@ public class Solution {
             tempNode = newNode;        //把新节点赋给临时变量tempNode
         }
         return sentineNode.next;
+    }
+
+    /**
+     * 思路：同时遍历两个链表，逐位计算它们的和，并与当前位置的进位值相加。直接转换成链表
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        boolean valBol = false;
+
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode tmpNode1 = l1.next;
+        ListNode tmpNode2 = l2.next;
+        int val = l1.val + l2.val;
+        if (val >= 10) {
+            valBol = true;
+            val %= 10;
+        }
+
+        ListNode sentinelNode = new ListNode(val);
+        ListNode tempNode = sentinelNode;
+        ListNode newNode;
+        while (tmpNode1 != null && tmpNode2 != null) {
+            int tmpVal = tmpNode1.val + tmpNode2.val;
+            if (valBol) {
+                tmpVal++;
+                valBol = false;
+            }
+            if (tmpVal >= 10) {
+                tmpVal %= 10;
+                valBol = true;
+            }
+            newNode = new ListNode(tmpVal);
+            tempNode.next = newNode;
+            tempNode = newNode;
+            tmpNode1 = tmpNode1.next;
+            tmpNode2 = tmpNode2.next;
+        }
+        if (tmpNode1 != null) {
+            while (tmpNode1 != null) {
+                int tmp = tmpNode1.val;
+
+                if (valBol) {
+                    tmp++;
+                    valBol = false;
+                }
+                if (tmp >= 10) {
+                    tmp -= 10;
+                    valBol = true;
+                }
+                newNode = new ListNode(tmp);
+                tempNode.next = newNode;
+                tempNode = newNode;
+
+                tmpNode1 = tmpNode1.next;
+            }
+        }
+        if (tmpNode2 != null) {
+            while (tmpNode2 != null) {
+                int tmp = tmpNode2.val;
+                if (valBol) {
+                    tmp++;
+                    valBol = false;
+                }
+                if (tmp >= 10) {
+                    tmp -= 10;
+                    valBol = true;
+                }
+                newNode = new ListNode(tmp);
+                tempNode.next = newNode;
+                tempNode = newNode;
+                tmpNode2 = tmpNode2.next;
+            }
+        }
+        if (valBol) {
+            newNode = new ListNode(1);
+            tempNode.next = newNode;
+            tempNode = newNode;
+        }
+        return sentinelNode;
     }
 }
 
