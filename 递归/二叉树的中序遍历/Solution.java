45acafee8be51a0003d93f1e87fcf0d0ac1bceb7
chenxuyang02 <chenxuyang02@58.com>
2021-10-14 11:48:12 +0800

    二叉树的中序遍历

--- 递归/二叉树的中序遍历/Solution.java
+++ 递归/二叉树的中序遍历/Solution.java
@@ -1,7 +1,7 @@
 package 二叉树的中序遍历;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @program: leetcode-collect
@@ -12,31 +12,42 @@ import java.util.Set;
 public class Solution {
 
     /**
-     * 思路：
+     * 思路：递归
      *
-     * @param s
+     * @param root
      * @return
      */
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer>
-    }
-}
-
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
+        List<Integer> result = new ArrayList<>();
+        centerOrder(root, result);
+        return result;
     }
 
-    TreeNode(int val) {
-        this.val = val;
+    private void centerOrder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        centerOrder(root.left, result);
+        result.add(root.val);
+        centerOrder(root.right, result);
     }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
